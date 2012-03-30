8c5c2b69c684fbe98f81d94ddc40d0d73797ee2a
Robey Pointer <robey@twitter.com>
2012-03-30 13:25:47 -0700

    sbt's "(package)-(scala_version)" standard now requires use of @DIST_NAME@.

--- lib/template/src/scripts/devel.sh
+++ lib/template/src/scripts/devel.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "Starting birdname in development mode..."
-java -server -Xmx1024m -Dstage=development -jar ./dist/birdname/birdname-@VERSION@.jar
+java -server -Xmx1024m -Dstage=development -jar ./dist/birdname/@DIST_NAME@-@VERSION@.jar
